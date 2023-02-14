e6ff0779c7f8717e11795ecacd95309861ec437d
nmusi <n.musinov@gmail.com>
2023-02-14 16:47:10 +0300

    update V3

--- src/main/resources/db/migration/V3__create_anonymous.sql
+++ src/main/resources/db/migration/V3__create_anonymous.sql
@@ -1,6 +1,6 @@
-insert into client("id", "name","second_name", "avatar", "email", "role" )
-values(1,"anonymous","anonymous","https://penfox.ru/wp-content/uploads/2022/01/Anonim_avatar-96x96.jpg",
-       "anonymous","USER");
+insert into client(id,name,second_name, avatar,email,role)
+values(1,'anonymous','anonymous','https://penfox.ru/wp-content/uploads/2022/01/Anonim_avatar-96x96.jpg',
+       'anonymous','USER');
 
 create table if not exists client
 (
