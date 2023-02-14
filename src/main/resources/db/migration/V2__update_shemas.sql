410db5a6b367771598f2688df01b9838299bb6fd
nmusi <n.musinov@gmail.com>
2023-02-14 17:05:55 +0300

    delete V3 migration

--- src/main/resources/db/migration/V2__update_shemas.sql
+++ src/main/resources/db/migration/V2__update_shemas.sql
@@ -12,7 +12,7 @@ create table if not exists client
     "id"          bigserial primary key,
     "name"        char(50)  not null,
     "second_name" char(50)  not null,
-    "avatar" char(50)  not null,
+    "avatar" char(255)  not null,
     "email" char(50)  not null,
     "role" char(50)  not null,
     "message_id"  bigint    null,
@@ -26,3 +26,7 @@ create table if not exists client
 
 create index if not exists client_tbl_role_id_idx on client
     (message_id);
+
+insert into client(id,name,second_name,avatar,email,role)
+values(1,'anonymous','anonymous','https://penfox.ru/wp-content/uploads/2022/01/Anonim_avatar-96x96.jpg',
+       'anonymous','USER');
--- src/main/resources/db/migration/V3__create_anonymous.sql
+++
@@ -1,4 +0,0 @@
-alter table client alter column avatar type varchar(255)  not null;
-insert into client(id,name,second_name,avatar,email,role)
-values(1,'anonymous','anonymous','https://penfox.ru/wp-content/uploads/2022/01/Anonim_avatar-96x96.jpg',
-       'anonymous','USER');
